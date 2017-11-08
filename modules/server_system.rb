ace759dc34972c7dc150b934f7962be6e1278772
Tim Engelhardt <timdaniel.engelhardt@web.de>
2017-11-08 11:56:52 +0100

    Added support for the new modules.

--- modules/server_system.rb
+++ modules/server_system.rb
@@ -1,5 +1,5 @@
-require_relative 'self_assigning_roles'
 require_relative 'join_leave_messages'
+require_relative '../lib/role_message'
 
 # Sets up and resets a whole bunch of stuff automatically
 module ServerSystem
@@ -7,79 +7,43 @@ module ServerSystem
 
   # Setting things up for every server the bot is on on startup.
   ready do |event|
-    event.bot.servers.each_value do |server|
-      init(event, server)
+    SSB.servers.each_value do |server|
+      create_server_table(server.id)
+      DB.init_default_values(server)
+      init_permissions(event, server)
+
+      message = RoleMessage.send(server)
+      RoleMessage.add_role_await(server, message)
     end
-    # Create & send the role assignment messages.
-    SelfAssigningRoles.init_role_message(event)
-    event.bot.game = ".help | NEW PREFIX!"
+
+    SSB.game = '.help'
+
     # Start the 24 hour cycle of refreshing the reactions on the role assignment messages.
     refresh_reactions_cycle(event)
   end
 
   server_create do |event|
     # Set up everything for the server the bot just joined.
-    init(event, event.server)
+    create_server_table(event.server.id)
+    DB.init_default_values(event.server)
   end
 
   server_delete do |event|
-    # Clean up the .yaml files when the bot gets kicked from a server.
-    delete_assigns(event.server)
-    delete_messages(event.server)
+    # Clean up the database when the bot gets kicked from a server.
+    DB.delete_server_values(event.server.id)
   end
 
-  def self.init(event, server)
-    # Sets up the stores and the staff role for a given server.
-    init_assigns(server)
-    init_messages(server)
-    init_permissions(event, server)
-  end
-
-  private_class_method def self.init_assigns(server)
-    SelfAssigningRoles.assignable_roles_store.transaction do
-      # Assignment channel defaults to the rules channel...
-      assignment_channel = server.channels.find { |channel| channel.name.include?('rules') }
-      # ...or the top channel, if there's no rules channel.
-      assignment_channel ||= server.channels.sort_by { |c| [c.position, c.id] }[1]
-
-      # Log channel defaults to whatever convenient non-public channel it can find.
-      log_channel = server.channels.find { |channel| channel.name =~ /(mod|admin|staff|log)/i }
-      log_id = log_channel.id if log_channel
-
-      # Create the section for the server in the file, if it doesn't exist yet.
-      SelfAssigningRoles.assignable_roles_store[server.id] ||= {
-        self_assigning_roles: [],
-        log_channel: log_id,
-        assignment_channel: assignment_channel.id
-      }
-    end
-  end
-
-  private_class_method def self.init_messages(server)
-    JoinLeaveMessages.message_store.transaction do
-      # Create the section for the server in the file, if it doesn't exist yet.
-      JoinLeaveMessages.message_store[server.id] ||= {
-        join_message: [],
-        leave_message: []
-      }
-    end
-  end
-
+  # Sets up  the staff role for a given server.
   private_class_method def self.init_permissions(event, server)
-    event.bot.set_role_permission(server.roles.find { |role| role.name == 'BotCommand' }.id, 1)
+    SSB.set_role_permission(server.roles.find { |role| role.name == 'BotCommand' }.id, 1)
   rescue StandardError
     # If it doesn't find the BotCommand role, it creates it.
     bot_command = server.create_role
     bot_command.name = 'BotCommand'
-    server.owner.pm "I went ahead and created a 'BotCommand' role on '#{server.name}'. "\
-          "Since that's how I know who may use staff commands, you might want to move it up a bit."
-    init_permissions(event, server) # Needed so the permission level is actually set.
-  end
+    LOGGER.log "I went ahead and created a 'BotCommand' role on '#{server.name}'. "\
+      "Since that's how I know who may use staff commands, you might want to move it up a bit."
 
-  private_class_method def self.delete_assigns(server)
-    SelfAssigningRoles.assignable_roles_store.transaction do
-      SelfAssigningRoles.assignable_roles_store.delete(server.id)
-    end
+    init_permissions(event, server) # Needed so the permission level is actually set.
   end
 
   private_class_method def self.delete_messages(server)
@@ -89,10 +53,22 @@ module ServerSystem
   end
 
   private_class_method def self.refresh_reactions_cycle(event)
-    sleep 86400 # 24 hours
-    event.bot.servers.each_value do |server|
+    sleep 86_400 # 24 hours
+    SSB.servers.each_value do |server|
       SelfAssigningRoles.refresh_reactions(event, server)
     end
     refresh_reactions_cycle(event)
+  end
+
+  private_class_method def self.create_server_table(server_id)
+    attrs = {
+      log_channel: Integer,
+      role_message_id: Integer,
+      assignable_roles: Integer,
+      assignment_channel: Integer,
+      join_message: String,
+      leave_message: String
+    }
+    DB.create_table("ssb_server_#{server_id}".to_sym, attrs)
   end
 end
--- sharkshock_bot.rb
+++ sharkshock_bot.rb
@@ -5,52 +5,59 @@ require 'yaml/store'
 require_relative 'lib/emojis'
 require_relative 'lib/database'
 require_relative 'lib/reactions'
+require_relative 'lib/ssb_logger'
 require_relative 'lib/charts/chart'
 
 require_relative 'modules/mentions'
 require_relative 'modules/misc_commands'
 require_relative 'modules/server_system'
 require_relative 'modules/chart_commands'
+require_relative 'modules/logger_commands'
 require_relative 'modules/join_leave_messages'
-require_relative 'modules/self_assigning_roles'
+require_relative 'modules/assignment_commands'
 
 # Bot inv: https://discordapp.com/oauth2/authorize?&client_id=346043915142561793&scope=bot&permissions=2146958591
 
 # TODO: Improve .help (split normal / staff commands)
 # TODO: Upgrade from YAML files to MySQL database
-# TODO: Logger class
+# TODO: Maybe look into rufus-scheduler for scheduling
 
 login = YAML.load_file('.login')
-$bot_id = login[:client_id]
+BOT_ID = login[:client_id]
 
-# Initiates connection to the database
+# Create the directory that charts get saved in.
+Dir.mkdir('images') unless File.exist?('images')
+
 print 'Initiating database connection...'
 DB = Database.new(
-  server: login[:server_name], # Delete if ran on the same server like the database
+  server: login[:server_name], # Delete if ran on the same server as the database
   username: login[:username],
-  server_password: login[:server_password], # Delete if ran on the same server like the database
+  server_password: login[:server_password], # Delete if ran on the same server as the database
   db_name: login[:db_name],
   db_password: login[:db_password]
 )
 puts 'done!'
 
-BOT = Discordrb::Commands::CommandBot.new(
+SSB = Discordrb::Commands::CommandBot.new(
   token: login[:token],
-  client_id: $bot_id,
+  client_id: BOT_ID,
   prefix: '.',
   help_command: :help
 )
 
-BOT.include! Mentions
-BOT.include! ServerSystem
-BOT.include! MiscCommands
-BOT.include! ChartCommands
-BOT.include! JoinLeaveMessages
-BOT.include! SelfAssigningRoles
+LOGGER = SSBLogger.new
+
+SSB.include! Mentions
+SSB.include! ServerSystem
+SSB.include! MiscCommands
+SSB.include! ChartCommands
+SSB.include! LoggerCommands
+SSB.include! JoinLeaveMessages
+SSB.include! AssignmentCommands
 
 at_exit do
   DB.close
-  BOT.stop
+  SSB.stop
 end
 
-BOT.run
+SSB.run
