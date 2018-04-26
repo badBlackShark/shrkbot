e8b481ea4dbb80db249c70431d1e6483346a70fc
badBlackShark <timdaniel.engelhardt@web.de>
2018-04-26 18:23:35 +0200

    Added a method for a confirm dialog.

--- lib/reactions.rb
+++ lib/reactions.rb
@@ -32,4 +32,34 @@ module Reactions
   def self.error(message)
     react(message, Emojis.name_to_unicode('crossmark'))
   end
+
+  # Used to put an accept / decline dialog on a message. Gets the user that the prompt is for.
+  # Returns true / false depending on input, or nil if no choice was made
+  def self.yes_no(message, user)
+    choice = nil
+
+    SHRK.add_await(:"yes_no_#{message.id}", Discordrb::Events::ReactionAddEvent) do |r_event|
+      # Only the user who sent the message should be abled to confirm / deny.
+      next false unless (r_event.message.id == message.id) && (r_event.user.id == user.id)
+      choice = false if r_event.emoji.name == Emojis.name_to_emoji('crossmark')
+      choice = true if r_event.emoji.name == Emojis.name_to_emoji('checkmark')
+
+      next false if choice.nil?
+    end
+    spam_reactions(message, [Emojis.name_to_emoji('checkmark'), Emojis.name_to_emoji("crossmark")])
+
+    # Timeout
+    i = 30
+    loop do
+      if i.zero?
+        SHRK.awaits.delete(:"yes_no_#message.id")
+        return nil
+      elsif choice.nil?
+        i -= 1
+        sleep 1
+      else
+        return choice
+      end
+    end
+  end
 end
