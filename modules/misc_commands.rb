99bf918ea3a4ebc3dcd7155f4e34112b36b99dd8
badBlackShark <timdaniel.engelhardt@web.de>
2018-05-07 03:47:09 +0200

    Added an eval command.

--- modules/misc_commands.rb
+++ modules/misc_commands.rb
@@ -3,7 +3,6 @@ module MiscCommands
   extend Discordrb::EventContainer
   extend Discordrb::Commands::CommandContainer
 
-
   message(with_text: 'ping', permission_level: 2, permission_message: false) do |event|
     # next unless event.user.id == 94558130305765376
     message = event.respond 'fuck off'
@@ -35,5 +34,33 @@ module MiscCommands
       embed.image = Discordrb::Webhooks::EmbedImage.new(url: 'https://cdn.discordapp.com/attachments/345748230816006156/347070498078851103/Eternally_screaming.gif')
     end
     event.message.delete
+  end
+
+  attrs = {
+    permission_level: 2,
+    permission_message: false,
+    usage: '.eval ```rb<code>```',
+    description: 'Executes the given codeblock.'
+  }
+  command :eval, attrs do |event, *args|
+    code = args.join(' ').gsub(/```(rb)?/, '')
+    begin
+      output = eval(code)
+      embed = Discordrb::Webhooks::Embed.new
+      embed.add_field(
+        name: 'Input',
+        value: code.prepend("```rb\n") << '```'
+      )
+      embed.add_field(
+        name: 'Output',
+        value: output || '-'
+      )
+      embed.color = 1
+      embed.title = 'Evaluation of code.'
+      event.channel.send_embed('', embed)
+    rescue Exception => e
+      "An error occured while evaluation your code: "\
+      "```#{e}``` at ```#{e.backtrace.join("\n")[0..1800].gsub(/\s\w+\s*$/, '...')}```"
+    end
   end
 end
