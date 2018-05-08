6424f66c4addf98701aebc5437314e739b281bbb
badBlackShark <timdaniel.engelhardt@web.de>
2018-05-08 12:32:00 +0200

    Reminders are now persistent through bot restarts.

--- modules/reminders.rb
+++ modules/reminders.rb
@@ -6,6 +6,46 @@ module Reminders
 
   @scheduler = Rufus::Scheduler.new
 
+  def self.init
+    DB.create_table(
+      'shrk_reminders',
+      user: :bigint,
+      message: :text,
+      at: String,
+      scheduled: String,
+      channel: :bigint,
+      pm: :boolean,
+      job_id: String
+    )
+    reschedule
+  end
+
+  # Reschedules reminders that are still in the database. Will immediately trigger reminders
+  # which are scheduled for a time in the past.
+  private_class_method def self.reschedule
+    reminders = DB.read_all(:shrk_reminders)
+    reminders.each do |reminder|
+      if Time.parse(reminder[:at]) < Time.now
+        send_reminder(
+          SHRK.user(reminder[:user]),
+          SHRK.channel(reminder[:channel]),
+          Time.parse(reminder[:scheduled]),
+          reminder[:message],
+          reminder[:pm]
+        )
+        DB.delete_value(:shrk_reminders, :job_id, reminder[:job_id])
+      else
+        schedule_reminder(
+          SHRK.user(reminder[:user]),
+          SHRK.channel(reminder[:channel]),
+          reminder[:at],
+          reminder[:message],
+          reminder[:pm]
+        )
+      end
+    end
+  end
+
   attrs = {
     usage: 'remind <time> <message> <--pm>',
     description: "You will be reminded of <message> after <time>. Argument order doesn't matter. "\
@@ -18,13 +58,23 @@ module Reminders
     time = '1d' if time.empty?
 
     event.respond "I will remind you about `#{msg}` in #{time}."
-    @scheduler.in time do
-      if pm
-        event.user.pm "#{time} ago you wanted to be reminded about `#{msg}`."
-      else
-        event.respond "#{event.user.mention}, #{time} ago you wanted to be reminded about `#{msg}`."
-      end
-    end
+    job = schedule_reminder(event.user, event.channel, time, msg, pm)
+    DB.insert_row(:shrk_reminders, [event.user.id, msg, job.next_time.to_s, job.scheduled_at.to_s, event.channel.id, pm, job.id])
     nil
+  end
+
+  private_class_method def self.schedule_reminder(user, channel, time, msg, pm)
+    @scheduler.schedule time, job: true do |j|
+      DB.delete_value(:shrk_reminders, :job_id, j.id)
+      send_reminder(user, channel, j.scheduled_at, msg, pm)
+    end
+  end
+
+  private_class_method def self.send_reminder(user, channel, time, msg, pm)
+    if pm
+      user.pm "On #{time.strftime(TIME_FORMAT)} you wanted to be reminded about `#{msg}`."
+    else
+      channel.send "#{user.mention}, on #{time.strftime(TIME_FORMAT)} you wanted to be reminded about `#{msg}`."
+    end
   end
 end
