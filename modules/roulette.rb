1cd6bd8bbad17188bc55c4e64694fcffdc94f751
badBlackShark <timdaniel.engelhardt@web.de>
2018-05-22 09:44:32 +0200

    Roulette stats are now per server.

--- modules/roulette.rb
+++ modules/roulette.rb
@@ -4,13 +4,15 @@ require 'rufus-scheduler'
 # Revolver times out if people wait too long to play.
 module Roulette
   extend Discordrb::Commands::CommandContainer
+  extend self
 
   # ServerID => Role
   @survivor_role = {}
 
-  def self.init
+  def init
     DB.create_table(
       'shrk_roulette',
+      server: :bigint,
       user: :bigint,
       plays: Integer,
       streak: Integer,
@@ -18,12 +20,13 @@ module Roulette
       deaths: Integer
     )
 
-    # Converts the array of hashes to an ID => Hash hash.
-    @data = DB.read_all(:shrk_roulette).map { |e| [e[:user], e] }.to_h
+    # {User => {Server => {Attributes}}}
+    @data = DB.read_all(:shrk_roulette).group_by { |e| e[:user] }.transform_values { |v| v.map { |e| [e[:server], e] }.to_h }
 
     SHRK.servers.each_value do |server|
       role = server.roles.find { |r| r.name.eql?('Survivor') }
       unless role
+        # extract
         role = server.create_role(
           name: 'Survivor',
           colour: 13938487,
@@ -54,9 +57,9 @@ module Roulette
   }
   command :roulette, attrs do |event, flag|
     # Just so nothing is nil
-    init_user_data(event.user.id)
-    @data[event.user.id][:plays] += 1
-    @data[event.user.id][:modified] = true
+    init_user_data(event.user.id, event.server.id)
+    @data[event.user.id][event.server.id][:plays] += 1
+    @data[event.user.id][event.server.id][:modified] = true
 
     outcome = @revolver[event.server.id][@position[event.server.id]]
 
@@ -86,7 +89,7 @@ module Roulette
       embed = Discordrb::Webhooks::Embed.new
       embed.footer = {
         text: "Unlucky. #{event.user.name} has died on shot ##{pos}.\n"\
-               "You just ended your streak of #{@data[event.user.id][:streak]}."
+               "You just ended your streak of #{@data[event.user.id][event.server.id][:streak]}."
       }
       embed.colour = 12648448
       msg = WH.send(
@@ -96,8 +99,8 @@ module Roulette
         avatar_url: Icons.name_to_link("revolver_d#{pos}".to_sym),
         embed: embed
       )
-      @data[event.user.id][:deaths] += 1
-      @data[event.user.id][:streak] = 0
+      @data[event.user.id][event.server.id][:deaths] += 1
+      @data[event.user.id][event.server.id][:streak] = 0
       msg = JSON.parse(msg)['id']
       queue_for_deletion(
         event.server.id, [
@@ -108,11 +111,11 @@ module Roulette
       nil
     else
       @position[event.server.id] += 1
-      @data[event.user.id][:streak] += 1
+      @data[event.user.id][event.server.id][:streak] += 1
       embed = Discordrb::Webhooks::Embed.new
-      if @data[event.user.id][:highscore] < @data[event.user.id][:streak]
-        @data[event.user.id][:highscore] += 1
-        if @data[event.user.id][:highscore] == 20
+      if @data[event.user.id][event.server.id][:highscore] < @data[event.user.id][event.server.id][:streak]
+        @data[event.user.id][event.server.id][:highscore] += 1
+        if @data[event.user.id][event.server.id][:highscore] == 20
           award_survivor_role(event.user.id)
           embed.description = '**Survior role awarded!**'
         end
@@ -120,7 +123,7 @@ module Roulette
       end
       embed.footer = {
         text: "#{event.user.name} survived shot ##{@position[event.server.id]}.\n"\
-               "Your current streak is #{@data[event.user.id][:streak]}."
+               "Your current streak is #{@data[event.user.id][event.server.id][:streak]}."
       }
       embed.colour = 171520
       msg = WH.send(
@@ -142,38 +145,34 @@ module Roulette
   end
 
   attrs = {
-    usage: 'leaderboard',
-    description: 'Shows a leaderboard of top 10 roulette highscores, plays and deaths.'\
-                 "\nWARNING: Takes a while when called for the first time after a restart."
+    usage: 'leaderboard <--global>',
+    description: "Shows a leaderboard of top 10 roulette highscores, plays and deaths for this server.\n"\
+                 'If the `--global` flag is set, the leaderboard for all servers will be displayed.'
   }
-  command :leaderboard, attrs do |event|
-    embed = Discordrb::Webhooks::Embed.new
-    # Don't select more than the top 12. Reverse because sort sorts ascending.
-    highscores = @data.sort_by { |_, e| e[:highscore] }.reverse[0, 10]
-    deaths = @data.sort_by { |_, e| e[:deaths] }.reverse[0, 10]
-    plays = @data.sort_by { |_, e| e[:plays] }.reverse[0, 10]
+  command :leaderboard, attrs do |event, flag = ''|
+    global = flag.casecmp?('--global')
 
-    highscores.map! { |user, e| "#{SHRK.user(user).distinct}: **#{e[:highscore]}**  \u200b\n" }
-    deaths.map! { |user, e| "#{SHRK.user(user).distinct}: **#{e[:deaths]}**\n" }
-    plays.map! { |user, e| "#{SHRK.user(user).distinct}: **#{e[:plays]}**  \u200b\n" }
+    embed = Discordrb::Webhooks::Embed.new
+    content = global ? global_board : server_board(event.server.id)
 
     embed.add_field(
       name: '__Top 10 (highscore)__',
-      value: highscores.join(),
+      value: content[:highscores],
       inline: true
     )
     embed.add_field(
       name: '__Top 10 (plays)__',
-      value: plays.join(),
+      value: content[:plays],
       inline: true
     )
     embed.add_field(
       name: '__Top 10 (deaths)__',
-      value: deaths.join(),
+      value: content[:deaths],
       inline: true
     )
     embed.colour = 13938487
     embed.timestamp = Time.now
+    embed.footer = {text: global ? 'Showing global leaderboard.' : 'Showing leaderboard for this server.'}
     WH.send(
       event.channel.id,
       nil,
@@ -185,25 +184,30 @@ module Roulette
   end
 
   attrs = {
-    usage: 'stats',
-    description: 'Shows your current roulette stats.'
+    usage: 'stats <--server>',
+    description: "Shows your current roulette stats for this server.\n"\
+                 'If the `--global` flag is set, your stats for all servers will be displayed.'
   }
-  command :stats, attrs do |event|
-    init_user_data(event.user.id)
+  command :stats, attrs do |event, flag = ''|
+    global = flag.casecmp?('--global')
+    init_user_data(event.user.id, event.server.id)
     embed = Discordrb::Webhooks::Embed.new
     embed.author = {
       name: "Stats for #{event.user.distinct}",
       icon_url: event.user.avatar_url
     }
-    stats = ''
-    stats << "• Total number of plays: **#{@data[event.user.id][:plays]}**\n"
-    stats << "• Total number of deaths: **#{@data[event.user.id][:deaths]}**\n"
-    stats << "• Current streak: **#{@data[event.user.id][:streak]}**\n"
-    stats << "• Best streak: **#{@data[event.user.id][:highscore]}**"
+    stats = global ? global_stats(event.user.id) : server_stats(event.user.id, event.server.id)
 
-    embed.description = stats
-    embed.colour = @data[event.user.id][:highscore] >= 20 ? 13938487 : 12632256
+    description = ''
+    description << "• Total number of plays: **#{stats[:plays]}**\n"
+    description << "• Total number of deaths: **#{stats[:deaths]}**\n"
+    description << "• Current#{global ? 'hightest ': ''} streak: **#{stats[:streak]}**\n"
+    description << "• Best streak: **#{stats[:highscore]}**"
+
+    embed.description = description
+    embed.colour = stats[:highscore] >= 20 ? 13938487 : 12632256
     embed.timestamp = Time.now
+    embed.footer = {text: global ? 'Showing global stats.' : 'Showing your stats for this server.'}
 
     WH.send(
       event.channel.id,
@@ -227,28 +231,84 @@ module Roulette
   end
 
   # Stores all modified rows in the database.
-  def self.write_to_db
-    @data.each_pair do |id, h|
-      if h[:modified]
-        # The :modified attribute doesn't need to be written into the database.
-        DB.update_row(:shrk_roulette, [id, h[:plays], h[:streak], h[:highscore], h[:deaths]])
-        h[:modified] = false
+  def write_to_db
+    @data.each_pair do |user_id, h|
+      h.each_pair do |server_id, a|
+        if a[:modified]
+          # The :modified attribute doesn't need to be written into the database.
+          DB.update_row_double_key(
+            :shrk_roulette,
+            [user_id, server_id, a[:plays], a[:streak], a[:highscore], a[:deaths]]
+          )
+          a[:modified] = false
+        end
       end
     end
   end
 
-  private_class_method def self.queue_for_deletion(server_id, msgs)
+  private
+
+  def global_board
+    # Don't select more than the top 10. Reverse because sort sorts ascending.
+    highscores = @data.transform_values { |e| e.values.max_by { |v| v[:highscore] } }
+                      .sort_by { |e, f| f[:highscore] }.reverse[0, 10]
+    deaths = @data.transform_values { |e| e.values.max_by { |v| v[:deaths] } }
+                  .sort_by { |e, f| f[:deaths] }.reverse[0, 10]
+    plays = @data.transform_values { |e| e.values.max_by { |v| v[:plays] } }
+                 .sort_by { |e, f| f[:plays] }.reverse[0, 10]
+
+    highscores.map! { |user, e| "• #{SHRK.user(user).distinct}: **#{e[:highscore]}**  \u200b\n" }
+    deaths.map! { |user, e| "• #{SHRK.user(user).distinct}: **#{e[:deaths]}**\n" }
+    plays.map! { |user, e| "• #{SHRK.user(user).distinct}: **#{e[:plays]}**  \u200b\n" }
+
+    {highscores: highscores.join(), deaths: deaths.join(), plays: plays.join()}
+  end
+
+  def server_board(server_id)
+    # Don't select more than the top 10. Reverse because sort sorts ascending.
+    highscores = @data.select { |e, h| h.keys.include?(server_id) }
+                      .transform_values { |e| e.select { |h, _| h == server_id } }
+                      .sort_by { |e, f| f[server_id][:highscore] }.reverse[0, 10]
+
+    deaths = @data.select { |e, h| h.keys.include?(server_id) }
+                  .transform_values { |e| e.select { |h, _| h == server_id } }
+                  .sort_by { |e, f| f[server_id][:deaths] }.reverse[0, 10]
+
+    plays = @data.select { |e, h| h.keys.include?(server_id) }
+                 .transform_values { |e| e.select { |h, _| h == server_id } }
+                 .sort_by { |e, f| f[server_id][:plays] }.reverse[0, 10]
+
+    highscores.map! { |user, e| "• #{SHRK.user(user).distinct}: **#{e[server_id][:highscore]}**  \u200b\n" }
+    deaths.map! { |user, e| "• #{SHRK.user(user).distinct}: **#{e[server_id][:deaths]}**\n" }
+    plays.map! { |user, e| "• #{SHRK.user(user).distinct}: **#{e[server_id][:plays]}**  \u200b\n" }
+
+    {highscores: highscores.join(), deaths: deaths.join(), plays: plays.join()}
+  end
+
+  def global_stats(user_id)
+    {
+      plays: @data[user_id].values.reduce(0) { |sum, e| sum += e[:plays] },
+      deaths: @data[user_id].values.reduce(0) { |sum, e| sum += e[:deaths] },
+      streak: @data[user_id].values.max_by { |v| v[:streak] }[:streak],
+      highscore: @data[user_id].values.max_by { |v| v[:highscore] }[:highscore]
+    }
+  end
+
+  def server_stats(user_id, server_id)
+    {
+      plays: @data[user_id][server_id][:plays],
+      deaths: @data[user_id][server_id][:deaths],
+      streak: @data[user_id][server_id][:streak],
+      highscore: @data[user_id][server_id][:highscore]
+    }
+  end
+
+  def queue_for_deletion(server_id, msgs)
     @messages[server_id].concat(msgs)
   end
 
-  private_class_method def self.award_survivor_role(id)
-    SHRK.servers.each_value do |server|
-      SHRK.user(id).on(server)&.add_role(@survivor_role[server.id])
-    end
-  end
-
   # Deletes the roulette call, as well as the bot's response
-  private_class_method def self.delete_round(server)
+  def delete_round(server)
     # Creates a hash channel_id => Array of message IDs in that channel.
     del = @messages[server.id].group_by { |h| h[:channel] }.each_value { |e| e.map! { |h| h[:id] } }
     @messages[server.id] = []
@@ -262,13 +322,20 @@ module Roulette
     end
   end
 
+  def award_survivor_role(id)
+    SHRK.servers.each_value do |server|
+      SHRK.user(id).on(server)&.add_role(@survivor_role[server.id])
+    end
+  end
+
   # Initialize with default values, so nothing we want to increment can be nil
-  private_class_method def self.init_user_data(id)
-    @data[id] ||= {}
-    @data[id][:plays] ||= 0
-    @data[id][:deaths] ||= 0
-    @data[id][:streak] ||= 0
-    @data[id][:highscore] ||= 0
+  def init_user_data(user_id, server_id)
+    @data[user_id] ||= {}
+    @data[user_id][server_id] ||= {}
+    @data[user_id][server_id][:plays] ||= 0
+    @data[user_id][server_id][:deaths] ||= 0
+    @data[user_id][server_id][:streak] ||= 0
+    @data[user_id][server_id][:highscore] ||= 0
   end
 
   def self.load_revolver(id)
