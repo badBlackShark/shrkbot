314110a491a0dfc3c0c845acdd03d67ce66e67a4
badBlackShark <timdaniel.engelhardt@web.de>
2018-05-29 08:58:02 +0200

    Now properly sets up permissions for new servers.

--- modules/server_system.rb
+++ modules/server_system.rb
@@ -11,7 +11,7 @@ module ServerSystem
       Thread.new do
         create_server_table(server.id)
         DB.init_default_values(server)
-        init_permissions(event, server)
+        init_permissions(server)
 
         message = RoleMessage.send(server)
         RoleMessage.add_role_await(server, message)
@@ -27,6 +27,7 @@ module ServerSystem
     # Set up everything for the server the bot just joined.
     create_server_table(event.server.id)
     DB.init_default_values(event.server)
+    init_permissions(event.server)
   end
 
   server_delete do |event|
@@ -35,7 +36,7 @@ module ServerSystem
   end
 
   # Sets up  the staff role for a given server.
-  private_class_method def self.init_permissions(event, server)
+  private_class_method def self.init_permissions(server)
     SHRK.set_role_permission(server.roles.find { |role| role.name == 'BotCommand' }.id, 1)
   rescue StandardError
     # If it doesn't find the BotCommand role, it creates it.
@@ -44,7 +45,7 @@ module ServerSystem
     LOGGER.log "I went ahead and created a 'BotCommand' role on '#{server.name}'. "\
       "Since that's how I know who may use staff commands, you might want to move it up a bit."
 
-    init_permissions(event, server) # Needed so the permission level is actually set.
+    init_permissions(server) # Needed so the permission level is actually set.
   end
 
   private_class_method def self.create_server_table(server_id)
