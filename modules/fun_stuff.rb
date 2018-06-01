f5432a125c200e29c7d6bab374508dc0cede93fc
PsykoKitFox <39064070+PsykoKitFox@users.noreply.github.com>
2018-05-31 21:06:09 -0500

    Added ayy lmao response

--- modules/fun_stuff.rb
+++ modules/fun_stuff.rb
@@ -14,4 +14,16 @@ module FunStuff
   command :intensify, attrs do |_event, *args|
     "*#{args.join(' ').split('').join(' ')}*"
   end
+
+  message(with_text: 'ping') do |event|
+    next unless SHRK.permission?(event.user, 2, event.server)
+    message = event.respond 'fuck off'
+    sleep 10
+    message.edit "I mean, 'pong'"
+    message.react(Emojis.name_to_unicode('heart'))
+  end
+
+  message(contains: /^ayy$/i) do |event|
+    event.respond 'lmao' if (1..5).to_a.sample == 1
+  end
 end
