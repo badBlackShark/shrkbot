0ca8624d77e960daf8c4c8f24c3b3f3ab003b950
badBlackShark <Timdaniel.Engelhardt@web.de>
2020-08-19 01:53:58 +0200

    Message now actually only sends to unique owners.

--- src/plugins/owner_message.cr
+++ src/plugins/owner_message.cr
@@ -13,13 +13,14 @@ class Shrkbot::OwnerMessage
     message = ctx[ArgumentChecker::Result].args.join(" ")
 
     guilds = Shrkbot.bot.cache.guilds.values
-    guilds.uniq.each do |guild|
+    unique_owners = guilds.uniq { |g| g.owner_id }
+    unique_owners.each do |guild|
       message += "\n\nYou are receiving this message because you're the owner of at least one server that this bot is on."
       client.create_message(client.create_dm(guild.owner_id).id, message)
       owner = Shrkbot.bot.cache.resolve_user(guild.owner_id)
       client.create_message(payload.channel_id, "Sent message to #{owner.username}##{owner.discriminator}, owner of *#{guild.name}*.")
     end
 
-    client.create_message(payload.channel_id, "Sent message to the #{guilds.uniq.size} owner(s) of #{guilds.size} guild(s).")
+    client.create_message(payload.channel_id, "Sent message to the #{unique_owners.size} owner(s) of #{guilds.size} guild(s).")
   end
 end
