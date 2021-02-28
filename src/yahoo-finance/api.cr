d88b787b496627a168faa20984203628806a8afd
badBlackShark <Timdaniel.Engelhardt@web.de>
2021-02-28 18:12:13 +0100

    Made the API call slightly more resilient.

--- src/yahoo-finance/api.cr
+++ src/yahoo-finance/api.cr
@@ -12,7 +12,11 @@ class YahooFinance::Api
 
   def get_chart(symbol : String, interval : String = "5m", range : String = "1d")
     response = request("/stock/v2/get-chart?interval=#{interval}&symbol=#{symbol}&range=#{range}&region=US")
-    return JSON.parse(response.body)
+    if response.status_code == 200
+      return JSON.parse(response.body)
+    else
+      raise "Error in request to Yahoo Finance, code #{response.status_code}"
+    end
   end
 
   private def request(endpoint : String)
