d246c3c2a3fa736d3d29f44d3fcf2c3536284b87
Tim Engelhardt <timdaniel.engelhardt@web.de>
2026-06-15 20:14:15 +0000

    Scaffold Rails 8 app, remove Crystal implementation
    Begins the Crystal/discordcr -> Ruby/Rails/discordrb rewrite.
    - rails new: Tailwind, importmap, pg, propshaft, Solid Queue/Cache/Cable
    - discordrb (git main) for the bot (require:false); OmniAuth Discord; redis; dotenv-rails; RSpec
    - Tom Select pinned via importmap (Node-free)
    - removed Crystal sources (src/, shard.*, spec/)

---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tom-select" # @2.6.1
+pin "@orchidjs/sifter", to: "@orchidjs--sifter.js" # @1.1.0
+pin "@orchidjs/unicode-variants", to: "@orchidjs--unicode-variants.js" # @1.1.2
--- src/helpers/permission_level.cr
+++
@@ -1,5 +0,0 @@
-enum PermissionLevel
-  User
-  Moderator
-  Creator
-end
